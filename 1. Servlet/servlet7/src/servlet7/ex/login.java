ae90d758926a709caa9e2bda408cc0f483b15c2b
zaehuun <51132077+zaehuun@users.noreply.github.com>
2020-06-26 05:43:41 +0900

    servlet6 + js를 사용하여 유효성 검사

---
+++ 1. Servlet/servlet7/src/servlet7/ex/login.java
@@ -0,0 +1,48 @@
+package servlet7.ex;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class login
+ */
+@WebServlet("/login")
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		String add = request.getParameter("address");
+		
+		System.out.println("ID : " + id);
+		System.out.println("PW : " + pw);
+		System.out.println("ADDRESS : " + add);
+	}
+
+}
