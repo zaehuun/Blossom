5a090752acfac2669cd24e5bad7309a50937d078
zaehuun <51132077+zaehuun@users.noreply.github.com>
2020-06-25 23:30:36 +0900

    HTML의 form과 input 태그를 사용하여 서블릿으로 데이터 전송

---
+++ 1. Servlet/servlet4/src/servlet4/ex/loginServlet.java
@@ -0,0 +1,48 @@
+package servlet4.ex;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class loginServlet
+ */
+@WebServlet("/login")
+public class loginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public loginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		String id = request.getParameter("user_id");
+		String pw = request.getParameter("user_pw");
+		String[] food = request.getParameterValues("food");
+		System.out.println("아이디 : " + id);
+		System.out.println("비밀번호 : " + pw);
+		for(String a : food)
+			System.out.println("음식 : "+ a);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
