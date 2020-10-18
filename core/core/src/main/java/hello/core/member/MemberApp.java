4807c3a75b6c09a5f530c3702048797a5392d522
zaehuun <51132077+zaehuun@users.noreply.github.com>
2020-10-18 22:40:44 +0900

    Add files via upload

--- core/core/src/main/java/hello/core/member/MemberApp.java
+++ core/core/src/main/java/hello/core/member/MemberApp.java
@@ -1,13 +1,21 @@
 package hello.core.member;
 
 import hello.core.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     //test
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+
+
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
         //MemberService memberService = new MemberServiceImpl();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService= applicationContext.getBean("memberService",MemberService.class);
+                                       //메서드 이름으로 등록 됨   //타입
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
