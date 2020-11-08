b23b8c76bbc5e897fb19af5d3d9dcf0c8d279afa
zaehuun <51132077+zaehuun@users.noreply.github.com>
2020-11-09 04:32:56 +0900

    Add files via upload

--- core/core/src/main/java/hello/core/AppConfig.java
+++ core/core/src/main/java/hello/core/AppConfig.java
@@ -16,7 +16,10 @@ import org.springframework.context.annotation.Configuration;
 //공연 기획자 느낌
 //spring이 싱글톤 객체로 만들어서 관리 함
 @Configuration
+//configuration 없으면 아래 빈들은 스프링 컨테이너에서 관리 x 
 public class AppConfig {
+    //현재 memberService와 orderService 두 곳에서 memberRepository 호출하는데 싱글톤이 깨질까?
+    //근데 테스트 코드 실행하면 다 같은 repository 인스턴스임
 
     @Bean
     //@Bean(name = "mm") 으로 변경 가능
